0624715d857f199dc498563b81dac41af053db16
shafikul <sagar.shunno@gmail.com>
2015-11-10 16:56:37 +0600

    Finish user edit, update, and destroy actions

--- Gemfile
+++ Gemfile
@@ -16,6 +16,9 @@ gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 gem	'bootstrap-sass',	'3.2.0.0'
+gem	'faker', '1.4.2'
+gem	'will_paginate', '3.0.7'
+gem	'bootstrap-will_paginate',	'0.0.10'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151110052920) do
+ActiveRecord::Schema.define(version: 20151110095802) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
     t.string   "string"
     t.string   "remember_digest"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
