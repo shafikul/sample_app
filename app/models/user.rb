0c8817f342f4bac88e9e97fc20f0d8a391e42b05
shafikul <sagar.shunno@gmail.com>
2015-11-17 19:09:58 +0600

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,12 @@ class User < ActiveRecord::Base
 
   attr_accessor	:remember_token, :activation_token, :reset_token
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+
+  has_many :followers, through: :passive_relationships, source: :follower
 
   before_save	{	self.email	=	email.downcase	}
   before_create :create_activation_digest
@@ -13,13 +19,12 @@ class User < ActiveRecord::Base
   has_secure_password
   validates	:password,	length:	{	minimum:	6	}
 
-  #	Returns	the	hash	digest	of	the	given	string.
   def	User.digest(string)
     cost	=	ActiveModel::SecurePassword.min_cost	?	BCrypt::Engine::MIN_COST	:
         BCrypt::Engine.cost
     BCrypt::Password.create(string,	cost:	cost)
   end
-  #	Returns	a	random	token.
+
   def	User.new_token
     SecureRandom.urlsafe_base64
   end
@@ -52,7 +57,20 @@ class User < ActiveRecord::Base
   end
 
   def	feed
-    Micropost.where("user_id	=	?",	id)
+    following_ids	=	"SELECT	followed_id	FROM	relationships	WHERE	follower_id	=	:user_id"
+    Micropost.where("user_id	IN	(#{following_ids}) OR	user_id	=	:user_id",	user_id:	id)
+  end
+
+  def follow(other_user)
+   active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
   end
 
   private
